71b78179135ecec7ddc96df4671fa75e5f0cacd6
Emna HOMRANI <ehomrani@sofrecom.fr>
2025-08-20 15:18:20 +0100

    Ajout du test FrontDeskdisconnectionmanagement

---
+++ cypress/e2e/FrontDeskdisconnectionmanagement.cy.js
@@ -0,0 +1,50 @@
+describe('FrontDeskdisconnectionmanagement Workflow', () => {
+    before(() => {
+      cy.loginAndSearch();
+    });
+  
+    it('Passe à FrontDeskdisconnectionmanagement', () => {
+      // Cliquer sur "Product Id"
+      cy.contains('div', 'Product Id', { timeout: 15000 })
+        .scrollIntoView()
+        .should('be.visible')
+        .click();
+    
+      // Cliquer sur "Front Desk disconnection management"
+      cy.contains('span', 'Front Desk disconnection management', { timeout: 10000 })
+        .should('be.visible')
+        .click();
+    
+      cy.wait(500);
+    
+      cy.xpath('/html/body/app-root/app-order-details/div/div[6]/app-offers-section/ngb-accordion/div/div[2]/div/div/app-activity-section/div[3]/div/form/div[1]/div[1]/div/app-disco-fd-activity/div/form/div/div/div/div/div/mat-checkbox/label/span[1]', { timeout: 15000 })
+  .should('be.visible')
+  .click({ force: true })
+  cy.wait(500);
+
+    
+     // 5. Liste déroulante "Statut"
+     cy.xpath(
+      '/html/body/app-root/app-order-details/div/div[6]/app-offers-section/ngb-accordion/div/div[2]/div/div/app-activity-section/div[3]/div/form/div[1]/div[2]/div/div[1]/mat-form-field/div/div[1]/div/mat-select/div/div[1]/span'
+    )
+      .should('be.visible')
+      .click({ force: true });
+
+    cy.get('.mat-select-panel', { timeout: 10000 })
+      .should('exist')
+      .should('not.have.class', 'ng-animating');
+
+    cy.get('.mat-option-text')
+      .contains('Completed')
+      .scrollIntoView()
+      .click({ force: true });
+
+    // 6. Cliquer sur "Save"
+    cy.contains('button', 'Save')
+      .should('be.visible')
+      .should('not.be.disabled')
+      .click();
+      cy.wait(500);
+    });
+    
+  });

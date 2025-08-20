9d618c580a03f9ba708ddfe363e0dc1931e6b625
Emna HOMRANI <ehomrani@sofrecom.fr>
2025-08-20 15:53:28 +0100

    Billingvalidation

---
+++ cypress/e2e/Billingvalidation.cy.js
@@ -0,0 +1,59 @@
+describe('Billingvalidationt Workflow', () => {
+    before(() => {
+      cy.loginAndSearch();
+    });
+  
+    it('Passe à Billingvalidation', () => {
+        cy.xpath(
+          '/html/body/app-root/app-order-details/div/div[5]/app-order-qualification/div/div[2]/div/div[2]/app-activity-section/div[2]/table/tbody/tr[7]/td[2]/span',
+          { timeout: 10000 }
+        )
+          .should('contain.text', 'Billing validation')
+          .should('be.visible')
+          .click();
+    
+        cy.wait(500);
+
+        // 1. Première liste déroulante
+cy.xpath(
+    '/html/body/app-root/app-order-details/div/div[5]/app-order-qualification/div/div[2]/div/div[2]/app-activity-section/div[3]/div/form/div[1]/div[1]/div/app-billing-validation-activity/div/form/div/div/div/div/mat-form-field/div/div[1]/div/mat-select/div/div[1]/span',
+    { timeout: 10000 })
+    .should('be.visible')
+    .click({ force: true });
+  
+  // Wait for the dropdown panel to be visible and not animating
+  cy.get('[formcontrolname="invoiceValidated"]', { timeout: 15000 })
+    .should('be.visible')
+    .should('not.have.class', 'ng-animating');
+  
+  // Select the "OK" option using the new XPath
+  cy.xpath('/html/body/div[2]/div[2]/div/div/div/mat-option[1]/span', { timeout: 10000 })
+    .should('be.visible')
+    .contains('OK') // Verify the text is "OK"
+    .click({ force: true });
+    
+     // 5. Liste déroulante "Statut"
+     cy.xpath(
+      '/html/body/app-root/app-order-details/div/div[5]/app-order-qualification/div/div[2]/div/div[2]/app-activity-section/div[3]/div/form/div[1]/div[2]/div/div[1]/mat-form-field/div/div[1]/div/mat-select/div/div[1]/span'
+    )
+      .should('be.visible')
+      .click({ force: true });
+
+    cy.get('.mat-select-panel', { timeout: 10000 })
+      .should('exist')
+      .should('not.have.class', 'ng-animating');
+
+    cy.get('.mat-option-text')
+      .contains('Completed')
+      .scrollIntoView()
+      .click({ force: true });
+
+    // 6. Cliquer sur "Save"
+    cy.contains('button', 'Save')
+      .should('be.visible')
+      .should('not.be.disabled')
+      .click();
+      cy.wait(500);
+    });
+    
+  });

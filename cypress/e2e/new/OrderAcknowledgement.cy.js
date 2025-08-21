0d5397674f68a81a3e62d9d13bdfe668ce34941e
Emna HOMRANI <ehomrani@sofrecom.fr>
2025-08-21 11:17:40 +0100

    Ajout du test OrderAcknowledgement

---
+++ cypress/e2e/new/OrderAcknowledgement.cy.js
@@ -0,0 +1,60 @@
+describe('Order Acknowledgement Workflow', () => {
+    before(() => {
+      cy.loginAndSearch();
+    });
+  
+    it('Passe à Order Acknowledgement', () => {
+      cy.xpath(
+        '/html/body/app-root/app-order-details/div/div[5]/app-order-qualification/div/div[2]/div/div[2]/app-activity-section/div[2]/table/tbody/tr[5]/td[2]/span',
+        { timeout: 10000 }
+      )
+        .should('contain.text', 'Order Acknowledgement')
+        .should('be.visible')
+        .click();
+  
+      cy.wait(500);
+  
+     // 2. Deuxième liste déroulante
+    cy.xpath(
+      '/html/body/app-root/app-order-details/div/div[5]/app-order-qualification/div/div[2]/div/div[2]/app-activity-section/div[3]/div/form/div[1]/div[1]/div/app-order-acknowledgement-activity/div/form/div/div/div/div/mat-form-field/div/div[1]/div/mat-select/div/div[1]/span',
+      { timeout: 10000 }
+    )
+      .should('be.visible')
+      .click({ force: true });
+    
+    // 2. Attendre que le panel d’options apparaisse dans le DOM
+    cy.get('[formcontrolname="orderAcknowledgementSent"]', { timeout: 15000 })
+    .should('be.visible')
+    .should('not.have.class', 'ng-animating');
+    
+    // 3. Cliquer sur l’option "Yes" en ciblant le span à l’intérieur du panel
+    cy.xpath('/html/body/div[2]/div[2]/div/div/div/mat-option[1]/span', { timeout: 15000 })
+  .contains('Yes') // Verify the text is "OK"
+  .click({ force: true });
+  
+  
+      // 5. Liste déroulante "Statut"
+      cy.xpath(
+        '/html/body/app-root/app-order-details/div/div[5]/app-order-qualification/div/div[2]/div/div[2]/app-activity-section/div[3]/div/form/div[1]/div[2]/div/div[1]/mat-form-field/div/div[1]/div/mat-select/div/div[1]/span'
+      )
+        .should('be.visible')
+        .click({ force: true });
+  
+      cy.get('.mat-select-panel', { timeout: 10000 })
+        .should('exist')
+        .should('not.have.class', 'ng-animating');
+  
+      cy.get('.mat-option-text')
+        .contains('Completed')
+        .scrollIntoView()
+        .click({ force: true });
+  
+      // 6. Cliquer sur "Save"
+      cy.contains('button', 'Save')
+        .should('be.visible')
+        .should('not.be.disabled')
+        .click();
+  
+      cy.wait(500);
+    });
+  });

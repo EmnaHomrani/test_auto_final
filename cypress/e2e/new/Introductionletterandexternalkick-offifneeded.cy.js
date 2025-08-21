2b06829b43b6e0c280d3ae6773cdbf5a39e9bc59
Emna HOMRANI <ehomrani@sofrecom.fr>
2025-08-21 11:25:05 +0100

    Ajout du test Introductionletterandexternalkick-offifneeded

---
+++ cypress/e2e/new/Introductionletterandexternalkick-offifneeded.cy.js
@@ -0,0 +1,41 @@
+describe('Introduction letter & external kick-off if needed Workflow', () => {
+    before(() => {
+      cy.loginAndSearch();
+    });
+  
+    it('Passe à Introduction letter & external kick-off if needed', () => {
+      cy.xpath(
+        '/html/body/app-root/app-order-details/div/div[5]/app-order-qualification/div/div[2]/div/div[2]/app-activity-section/div[2]/table/tbody/tr[6]/td[2]/span',
+        { timeout: 10000 }
+      )
+        .should('contain.text', 'Introduction letter & external kick-off if needed')
+        .should('be.visible')
+        .click();
+  
+      cy.wait(500);
+  
+      // 5. Liste déroulante "Statut"
+      cy.xpath(
+        '/html/body/app-root/app-order-details/div/div[5]/app-order-qualification/div/div[2]/div/div[2]/app-activity-section/div[3]/div/form/div[1]/div[2]/div/div[1]/mat-form-field/div/div[1]/div/mat-select/div/div[1]/span'
+      )
+        .should('be.visible')
+        .click({ force: true });
+  
+      cy.get('.mat-select-panel', { timeout: 10000 })
+        .should('exist')
+        .should('not.have.class', 'ng-animating');
+  
+      cy.get('.mat-option-text')
+        .contains('Completed')
+        .scrollIntoView()
+        .click({ force: true });
+  
+      // 6. Cliquer sur "Save"
+      cy.contains('button', 'Save')
+        .should('be.visible')
+        .should('not.be.disabled')
+        .click();
+  
+      cy.wait(500);
+    });
+  });

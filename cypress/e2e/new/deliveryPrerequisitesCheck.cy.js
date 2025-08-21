08b0781df2a0abcca9165ad786650f2c5cde0ab1
Emna HOMRANI <ehomrani@sofrecom.fr>
2025-08-21 02:24:36 +0100

    Ajout du test deliveryPrerequisitesCheck new non completed il reste un champs

---
+++ cypress/e2e/new/deliveryPrerequisitesCheck.cy.js
@@ -0,0 +1,57 @@
+describe('Delivery Prerequisites Check Workflow', () => {
+  before(() => {
+    cy.loginAndSearch();
+  });
+
+  it('Vérifie et complète Delivery prerequisites check', () => {
+    // Cliquer sur "Delivery prerequisites check"
+    cy.xpath(
+      '/html/body/app-root/app-order-details/div/div[5]/app-order-qualification/div/div[2]/div/div[2]/app-activity-section/div[2]/table/tbody/tr[2]/td[2]/span',
+      { timeout: 10000 }
+    )
+      .should('contain.text', 'Delivery prerequisites check')
+      .should('be.visible')
+      .click();
+
+    cy.wait(500);
+
+    function selectFirstOption(formControlName) {
+      cy.get(`[formcontrolname="${formControlName}"]`).click({ force: true })
+      cy.get('.cdk-overlay-container mat-option')
+        .first()
+        .click({ force: true })
+    }
+    
+    // Utilisation :
+    selectFirstOption('salesDoc')
+    selectFirstOption('preSalesDoc')
+    selectFirstOption('preSalesDocValueWorkable')
+    selectFirstOption('launchInProdRequired')
+    
+    
+
+    // 5. Liste déroulante "Statut"
+    cy.xpath(
+      '/html/body/app-root/app-order-details/div/div[5]/app-order-qualification/div/div[2]/div/div[2]/app-activity-section/div[3]/div/form/div[1]/div[2]/div/div[1]/mat-form-field/div/div[1]/div/mat-select/div/div[1]/span'
+    )
+      .should('be.visible')
+      .click({ force: true });
+
+    cy.get('.mat-select-panel', { timeout: 10000 })
+      .should('exist')
+      .should('not.have.class', 'ng-animating');
+
+    cy.get('.mat-option-text')
+      .contains('Completed')
+      .scrollIntoView()
+      .click({ force: true });
+
+    // 6. Cliquer sur "Save"
+    cy.contains('button', 'Save')
+      .should('be.visible')
+      .should('not.be.disabled')
+      .click();
+
+    cy.wait(500);
+  });
+});

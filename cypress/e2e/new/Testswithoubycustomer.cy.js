4276b83cdef698f611ad6cab3894430df71bf20d
Emna HOMRANI <ehomrani@sofrecom.fr>
2025-08-22 12:08:55 +0100

    Ajout du test Testswithoubycustomer.cy.js

---
+++ cypress/e2e/new/Testswithoubycustomer.cy.js
@@ -0,0 +1,52 @@
+describe('Tests with / by customer Workflow', () => {
+    before(() => {
+      cy.loginAndSearch();
+    });
+  
+    it('Passe à Tests with / by customer', () => {
+      // Cliquer sur "Product Id"
+      cy.contains('div', 'Product Id', { timeout: 15000 })
+        .scrollIntoView()
+        .should('be.visible')
+        .click();
+    
+      // Cliquer sur "Front Desk disconnection management"
+      cy.contains('span', 'Tests with / by customer', { timeout: 10000 })
+        .should('be.visible')
+        .click();
+    
+      cy.wait(500);
+
+    	    function selectFirstOption(formControlName) {
+      cy.get(`[formcontrolname="${formControlName}"]`).click({ force: true })
+      cy.get('.cdk-overlay-container mat-option')
+        .first()
+        .click({ force: true })
+    }
+    
+    // Utilisation :
+    
+    selectFirstOption('testsResults')
+    
+  // 5. Liste déroulante "Statut"
+cy.xpath(
+    '/html/body/app-root/app-order-details/div/div[6]/app-offers-section/ngb-accordion/div[1]/div[2]/div/div/app-activity-section/div[3]/div/form/div[1]/div[2]/div/div[1]/mat-form-field/div/div[1]/div/mat-select/div/div[1]/span'
+  )
+    .should('be.visible')
+    .click({ force: true });
+  
+  cy.get('.mat-option-text')
+    .contains('Completed')
+    .scrollIntoView()
+    .click({ force: true });
+  
+  // 6. Cliquer sur "Save"
+  cy.contains('button', 'Save')
+    .should('be.visible')
+    .should('not.be.disabled')
+    .click();
+
+  cy.wait(500);
+    });
+    
+  });
